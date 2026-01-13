1f137a15d46a8f5a4928c45b9e5511effe8d7d95
Pasin <pasin0931@gmail.com>
2026-01-13 13:56:44 +0700

    Add player movement animations

--- libs/sprite_lib.py
+++ libs/sprite_lib.py
@@ -23,8 +23,8 @@ class Player(pygame.sprite.Sprite):
         super(Player, self).__init__()
         self.sys = sys
         
-        self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255, 255, 255))
+        # self.surf = pygame.Surface((75, 25))
+        # self.surf.fill((255, 255, 255))
         
         self.surf = pygame.image.load("./pictures/player/mc.png").convert_alpha()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
@@ -41,7 +41,7 @@ class Player(pygame.sprite.Sprite):
         self.velo = self.jump_h
         self.g = 0.04
         self.on_ground = True
-        # self.jumping = False
+        self.jumping = False
         # -------------
         
         self.is_dashing = False
@@ -49,27 +49,54 @@ class Player(pygame.sprite.Sprite):
         self.dash_length = 150
         self.dash_speed = 1
         self.facing_L = False
-        self.facing_R = True
+        self.facing_R = False
         
+        self.is_moving = False
         # print(self.rect.x, self.rect.y)
         
     def update(self, pressed_keys, dashing, jump):
         # print(self.rect) # debug
+        self.is_moving = False
         
         # ---------- Jump
         if (pressed_keys[K_w] or pressed_keys[K_SPACE]) and self.on_ground and jump == True:
             self.velo = self.jump_h
             self.on_ground = False
+            self.jumping = True
+            
+        if not self.on_ground and self.jumping:
+            if self.facing_L and not self.facing_R:
+                self.surf = pygame.image.load("./pictures/player/anims/jumpl.png").convert_alpha()
+            elif not self.facing_L and self.facing_R:
+                self.surf = pygame.image.load("./pictures/player/anims/jumpr.png").convert_alpha()
         # ---------- Jump
         
         if pressed_keys[K_a]:
+            self.is_moving = True
+            if not self.jumping:
+                self.surf = pygame.image.load("./pictures/player/anims/runl.png").convert_alpha()
+            
             self.facing_L = True
             self.facing_R = False
             self.rect.move_ip(-1, 0) # speed left
-        if pressed_keys[K_d]:
+        elif pressed_keys[K_d]:
+            self.is_moving = True
+            if not self.jumping:
+                self.surf = pygame.image.load("./pictures/player/anims/runr.png").convert_alpha()
             self.facing_L = False
             self.facing_R = True
             self.rect.move_ip(1, 0) # speed right
+        elif pressed_keys[K_s]:
+            self.is_moving = False
+            self.facing_R = False
+            self.facing_L = False
+            self.surf = pygame.image.load("./pictures/player/mc.png").convert_alpha()
+        
+        if self.is_moving == False:   
+            if self.facing_L and not self.facing_R:
+                self.surf = pygame.image.load("./pictures/player/anims/facel.png").convert_alpha()
+            elif not self.facing_L and self.facing_R:
+                self.surf = pygame.image.load("./pictures/player/anims/facer.png").convert_alpha()
             
         # ---------- dash     
         if dashing and not self.is_dashing:
@@ -81,11 +108,13 @@ class Player(pygame.sprite.Sprite):
                   
         if self.is_dashing:
             if self.facing_L:
+                self.surf = pygame.image.load("./pictures/player/anims/roll_l.png").convert_alpha()
                 self.rect.x -= self.dash_speed
                 if self.rect.x <= self.dash_target:
                     self.rect.x = self.dash_target
                     self.is_dashing = False
             else:
+                self.surf = pygame.image.load("./pictures/player/anims/roll_r.png").convert_alpha()
                 self.rect.x += self.dash_speed
                 if self.rect.x >= self.dash_target:
                     self.rect.x = self.dash_target
@@ -103,5 +132,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= self.ground_location:
             self.rect.bottom = self.ground_location
+            self.jumping = False
             self.velo = 0
             self.on_ground = True
+            
+class attack:
+    def __init__(self, player):
+        self.player = player
